6e80acc8d0006b4492cdc3b7479a244c46d61450
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2021-08-05 22:47:28 +0300

    Adding cyrillic and capital letters accepting

--- unit_3_all_operations/src/main/java/ua/com/alevel/AmountOfLetters.java
+++ unit_3_all_operations/src/main/java/ua/com/alevel/AmountOfLetters.java
@@ -3,7 +3,6 @@ package ua.com.alevel;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Locale;
 
 public class AmountOfLetters {
     public void run() {
@@ -15,20 +14,58 @@ public class AmountOfLetters {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        message = message.toLowerCase();
         char[] chars = message.toCharArray();
         int[] alphabet = new int[26];
+        int[] alphabetBig = new int[26];
+        int[] alphabetKiril = new int[33];
+        int[] alphabetKirilBig = new int[33];
         for (int i = 0; i < chars.length; i++) {
             int num = chars[i];
             if (num >= 97 && num < 123) {
-                alphabet[num - 97] ++;
+                alphabet[num - 97]++;
+            } else if (num >= 65 && num < 91) {
+                alphabetBig[num - 65]++;
+            } else if (num >= 1040 && num < 1072) {
+                alphabetKirilBig[num - 1040]++;
+            } else if (num >= 1072 && num < 1104) {
+                alphabetKiril[num - 1072]++;
+            } else if (num == 1025) {
+                alphabetKirilBig[32]++;
+            } else if (num == 1105) {
+                alphabetKiril[32]++;
             }
         }
         int counter = 1;
-        for (int i = 0; i < alphabet.length; i++) {
-            if(alphabet[i] > 0) {
-                System.out.println(counter + ". " + (char)(i + 97) + " - " + alphabet[i]);
+        for (int i = 0; i < alphabetBig.length; i++) {
+            if(alphabetBig[i] > 0) {
+                System.out.println(counter + ". " + (char)(i + 65) + " - " + alphabetBig[i]);
                 counter++;
+            }
+        }
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] > 0) {
+                System.out.println(counter + ". " + (char) (i + 97) + " - " + alphabet[i]);
+                counter++;
+            }
+        }
+        for (int i = 0; i < alphabetKirilBig.length - 1; i++) {
+            if (alphabetKirilBig[i] > 0) {
+                System.out.println(counter + ". " + (char) (i + 1040) + " - " + alphabetKirilBig[i]);
+                counter++;
+                if (i == 5 && alphabetKirilBig[32] > 0) {
+                    System.out.println(counter + ". " + (char) (1025) + " - " + alphabetKirilBig[32]);
+                    counter++;
+                }
+            }
+        }
+        for (int i = 0; i < alphabetKiril.length - 1; i++) {
+            if (alphabetKiril[i] > 0) {
+                System.out.println(counter + ". " + (char) (i + 1072) + " - " + alphabetKiril[i]);
+                counter++;
+                if (i == 5 && alphabetKiril[32] > 0) {
+                    System.out.println(counter + ". " + (char) (1105) + " - " + alphabetKiril[32]);
+                    counter++;
+                }
             }
         }
     }
