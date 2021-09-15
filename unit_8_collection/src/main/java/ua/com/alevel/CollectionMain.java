0f9c092562f49306e266366471c0f8e32b0ea4bb
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2021-09-15 17:41:12 +0300

    Adding all methods

--- unit_8_collection/src/main/java/ua/com/alevel/CollectionMain.java
+++ unit_8_collection/src/main/java/ua/com/alevel/CollectionMain.java
@@ -11,5 +11,25 @@ public class CollectionMain {
         set.add(2);
         set.add(5, 2, 1);
         System.out.println(Arrays.toString(set.toArray()));
+        System.out.println(set.getMax());
+        System.out.println(set.getMin());
+        System.out.println(set.getAverage());
+        System.out.println(set.getMedian());
+        System.out.println(Arrays.toString(set.toArray(1, 3)));
+        System.out.println(Arrays.toString(set.cut(2, 3).toArray()));
+        set.clear(new Number[]{5, 1});
+        System.out.println(Arrays.toString(set.toArray()));
+        set.add(5, 1, 7, 3, 5, 3, 5, 7, 5, 5, 8, 9, 0, 23, 6, 21, 63, 15, 19);
+        System.out.println(Arrays.toString(set.toArray()));
+        set.sortAsc();
+        System.out.println(Arrays.toString(set.toArray()));
+        set.sortDesc();
+        System.out.println(Arrays.toString(set.toArray()));
+        set.sortAsc(7);
+        System.out.println(Arrays.toString(set.toArray()));
+        set.sortDesc(0, 5);
+        System.out.println(Arrays.toString(set.toArray()));
+        set.intersection(new MathSet(new Number[] {9, 1, 4, 5}));
+        System.out.println(Arrays.toString(set.toArray()));
     }
 }
--- unit_8_collection/src/main/java/ua/com/alevel/MathSet.java
+++ unit_8_collection/src/main/java/ua/com/alevel/MathSet.java
@@ -52,7 +52,7 @@ public class MathSet {
     }
 
     public void add(Number n) {
-        if (!contains(n)) {
+        if (!contains(set, n)) {
             if (capacity > size) {
                 set[size] = n;
                 size++;
@@ -60,6 +60,9 @@ public class MathSet {
                 Number[] temp = set;
                 set = new Number[(capacity * 3 / 2) + 1];
                 capacity = (capacity * 3 / 2) + 1;
+                for (int i = 0; i < size; i++) {
+                    set[i] = temp[i];
+                }
                 add(n);
             }
         }
@@ -71,16 +74,75 @@ public class MathSet {
         }
     }
 
-    public void join(MathSet ms) { }
-    public void join(MathSet ... ms) { }
-    public void intersection(MathSet ms) { }
-    public void intersection (MathSet ... ms) { }
-    public void sortDesc() { }
-    public void sortDesc(int firstIndex, int lastIndex) { }
-    public void sortDesc(Number value) { }
-    public void sortAsc() { }
-    public void sortAsc(int firstIndex, int lastIndex) { }
-    public void sortAsc(Number value) { }
+    public void join(MathSet ms) {
+        Number[] temp = ms.toArray();
+        for (Number num: temp) {
+            add(num);
+        }
+    }
+
+    public void join(MathSet ... ms) {
+        for (MathSet mathSet: ms) {
+            join(ms);
+        }
+    }
+
+    public void intersection(MathSet ms) {
+        Number[] temp = ms.toArray();
+        MathSet mathSet = new MathSet();
+        for (int i = 0; i < size; i++) {
+            if(!contains(temp, set[i])) {
+                mathSet.add(set[i]);
+            }
+        }
+        clear(mathSet.toArray());
+    }
+
+    public void intersection (MathSet ... ms) {
+        for (MathSet set: ms) {
+            intersection(ms);
+        }
+    }
+
+    public void sortDesc() {
+        quickSortDesc(set, 0, size - 1);
+    }
+
+    public void sortDesc(int firstIndex, int lastIndex) {
+        if (firstIndex <= lastIndex && firstIndex >= 0 && lastIndex < size) {
+            quickSortDesc(set, firstIndex, lastIndex);
+        } else {
+            System.out.println("Wrong indexes");
+        }
+    }
+
+    public void sortDesc(Number value) {
+        if (getIndex(value) != -1) {
+            quickSortDesc(set, getIndex(value), size - 1);
+        } else {
+            System.out.println("No such element");
+        }
+    }
+
+    public void sortAsc() {
+        quickSortAsc(set, 0, size - 1);
+    }
+
+    public void sortAsc(int firstIndex, int lastIndex) {
+        if (firstIndex <= lastIndex && firstIndex >= 0 && lastIndex < size) {
+            quickSortAsc(set, firstIndex, lastIndex);
+        } else {
+            System.out.println("Wrong indexes");
+        }
+    }
+
+    public void sortAsc(Number value) {
+        if (getIndex(value) != -1) {
+            quickSortAsc(set, getIndex(value), size - 1);
+        } else {
+            System.out.println("No such element");
+        }
+    }
 
     public Number get(int index) {
         if (index >= size || index < 0) {
@@ -91,10 +153,63 @@ public class MathSet {
         }
     }
 
-    public Number getMax() { return null; }
-    public Number getMin() { return null; }
-    public Number getAverage() { return null; }
-    public Number getMedian() { return null; }
+    public Number getMax() {
+        if (size > 0) {
+            Number max = set[0];
+            for (int i = 1; i < size; i++) {
+                if (set[i].doubleValue() > max.doubleValue()) {
+                    max = set[i];
+                }
+            }
+            return max;
+        } else {
+            System.out.println("There is no elements");
+            return null;
+        }
+    }
+
+    public Number getMin() {
+        if (size > 0) {
+            Number min = set[0];
+            for (int i = 1; i < size; i++) {
+                if (set[i].doubleValue() < min.doubleValue()) {
+                    min = set[i];
+                }
+            }
+            return min;
+        } else {
+            System.out.println("There is no elements");
+            return null;
+        }
+    }
+
+    public Number getAverage() {
+        if (size > 0) {
+            double sum = 0.0;
+            for (int i = 0; i < size; i++) {
+                sum = sum + set[i].doubleValue();
+            }
+            return sum / size;
+        } else {
+            System.out.println("There is no elements");
+            return null;
+        }
+    }
+
+    public Number getMedian() {
+        if (size > 0) {
+            Number[] temp = toArray();
+            quickSortAsc(temp, 0, size - 1);
+            if (size % 2 == 0) {
+                return (temp[size / 2 - 1].doubleValue() + temp[size / 2].doubleValue()) / 2;
+            } else {
+                return temp[size / 2 + 1];
+            }
+        } else {
+            System.out.println("There is no elements");
+            return null;
+        }
+    }
 
     public Number[] toArray() {
         if (capacity == size) {
@@ -108,8 +223,31 @@ public class MathSet {
         }
     }
 
-    public Number[] toArray(int firstIndex, int lastIndex) { return null; }
-    public MathSet cut(int firstIndex, int lastIndex) { return null; }
+    public Number[] toArray(int firstIndex, int lastIndex) {
+        if (firstIndex <= lastIndex && firstIndex >= 0 && lastIndex < size) {
+            Number[] temp = new Number[lastIndex - firstIndex + 1];
+            for (int i = firstIndex, j = 0; i <= lastIndex; i++, j++) {
+                temp[j] = set[i];
+            }
+            return temp;
+        } else {
+            System.out.println("Wrong indexes");
+            return null;
+        }
+    }
+
+    public MathSet cut(int firstIndex, int lastIndex) {
+        if (firstIndex <= lastIndex && firstIndex >= 0 && lastIndex < size) {
+            Number[] temp = new Number[lastIndex - firstIndex + 1];
+            for (int i = firstIndex, j = 0; i <= lastIndex; i++, j++) {
+                temp[j] = set[i];
+            }
+            return new MathSet(temp);
+        } else {
+            System.out.println("Wrong indexes");
+            return null;
+        }
+    }
 
     public void clear() {
         set = new Number[10];
@@ -117,14 +255,115 @@ public class MathSet {
         size = 0;
     }
 
-    public void clear(Number[] numbers) { }
+    public void clear(Number[] numbers) {
+        for (Number num: numbers) {
+            int initSize = size;
+            for (int i = 0; i < initSize; i++) {
+                if (num != null && set[i].doubleValue() == num.doubleValue()) {
+                    delete(i);
+                    break;
+                }
+            }
+        }
+    }
 
-    private boolean contains(Number n) {
-        for (Number num: set) {
-            if (num != null && num.doubleValue() == n.doubleValue()) {
-                return true;
+    private boolean contains(Number[] array, Number n) {
+        if (n != null) {
+            for (Number num: array) {
+                if (num != null && num.doubleValue() == n.doubleValue()) {
+                    return true;
+                }
             }
         }
         return false;
+    }
+
+    private void delete(int index) {
+        for (int i = index; i < size - 1; i++) {
+            set[i] = set[i + 1];
+        }
+        set[size - 1] = null;
+        size--;
+    }
+
+    private void quickSortAsc(Number[] nums, int low, int high) {
+        if (nums.length == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        Number base = nums[middle];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (nums[i].doubleValue() < base.doubleValue()) {
+                i++;
+            }
+
+            while (nums[j].doubleValue() > base.doubleValue()) {
+                j--;
+            }
+
+            if (i <= j) {
+                Number temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSortAsc(nums, low, j);
+
+        if (high > i)
+            quickSortAsc(nums, i, high);
+    }
+
+    private void quickSortDesc(Number[] nums, int low, int high) {
+        if (nums.length == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        Number base = nums[middle];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (nums[i].doubleValue() > base.doubleValue()) {
+                i++;
+            }
+
+            while (nums[j].doubleValue() < base.doubleValue()) {
+                j--;
+            }
+
+            if (i <= j) {
+                Number temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSortDesc(nums, low, j);
+
+        if (high > i)
+            quickSortDesc(nums, i, high);
+    }
+
+    private int getIndex(Number n) {
+        for (int i = 0; i < size; i++) {
+            if (set[i].doubleValue() == n.doubleValue()) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
