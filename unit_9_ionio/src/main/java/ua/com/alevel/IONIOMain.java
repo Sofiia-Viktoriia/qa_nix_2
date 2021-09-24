6e618f271a50dac2751fd49b8c8361fc588037b8
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2021-09-24 14:06:30 +0300

    Adding files and main function

---
+++ unit_9_ionio/src/main/java/ua/com/alevel/IONIOMain.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import ua.com.alevel.controller.IONIOController;
+
+public class IONIOMain {
+
+    public static void main(String[] args) {
+        new IONIOController().run();
+    }
+}
